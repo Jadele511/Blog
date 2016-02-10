23abfb46db6a64d26afd47d448dd5ffbc55ffbff
Jade Le <baongocle1991@gmail.com>
2016-02-10 16:01:41 +0700

    Added tag

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   
   resources :articles do
     resources :comments
- end
+  end
+  resources :tags
+
   
 
   # The priority is based upon order of creation: first created -> highest priority.
